58c4fca102842d7c1d71405475f46d1125c5765d
Adarsh <adarshanup.nz@gmail.com>
2022-11-26 11:12:17 +1000

    changes javascript test file

--- tests.js
+++ tests.js
@@ -1,30 +1,24 @@
-console.log('testing');
-let x = Date.now();
-let y = new Date();
-console.log(x)
 
-//console.log(Date.UTC(2021, 10, 16, 17, 0))
-
-//console.log(Date.UTC(2021, 10, 16, 5, 40))
-
-console.log(y.getFullYear(), y.getMonth(), y.getDate(), y.getHours(), y.getMinutes())
-
-console.log(Date.UTC(y.getFullYear(), y.getMonth(), y.getDate(), y.getHours(), y.getMinutes()))
-
-console.log(y.getUTCFullYear(), y.getUTCMonth(), y.getUTCDate(), y.getUTCHours(), y.getUTCMinutes(), y.getUTCSeconds(), y.getUTCMilliseconds())
-
-let a = 
-console.log(Date.UTC(y.getUTCFullYear(), y.getUTCMonth(), y.getUTCDate(), y.getUTCHours(), y.getUTCMinutes(), y.getUTCSeconds(), y.getUTCMilliseconds()))
- 
-let z = new Date(x)
-console.log(z.getHours(), z.getUTCHours());
-//console.log(Date.UTC())
-//console.log(x.getMonth());
-/*
-let testObject = {
-    function: testfunction,
-    list: {
-        entry: [key / description, timestamp],
-    }
+const {fetch} = require("node-fetch")
+let test = {
+    lol: 'test'
 }
-*/
+
+console.log(test);
+
+exports.testFunction = async () => {
+    const url = 'https://httpbin.org/post';
+    const body = JSON.stringify({ test });
+    const options = {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+            'Content-Length': body.length
+        },
+        body: body
+    };
+    let response = await fetch(url, options);
+    console.log('Got response from receipt callback url:', response);
+}
+
+this.testFunction();
